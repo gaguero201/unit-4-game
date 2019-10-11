1f23a4105e5ff32c97e856356df82751369b563c
gaguero201 <47278436+gaguero201@users.noreply.github.com>
2019-10-11 00:21:33 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,151 +1,139 @@
 $(document).ready(function() {
 
-	// ******* NUMBER ARRAYS *******
-
-	// random computer variable array
-	var rand = [];
-
-	for (var r = 19; r < 121; r++) {
-		rand.push(r);
-	}
-
-	// crystal numbers array
-	var crystals = [];
-
-	for (var c = 1; c < 13; c++) {
-
-		crystals.push(c);
-	}
-
-	// console.log(crystals);
-
-	// ******* GLOBAL VARIABLES *******
-
-	// random variables selected by computer
-	var randNumber; // number to match
-	var crystalNumbers = []; // for array of random crystal values
-
-	var c1;
-	var c2;
-	var c3;
-	var c4;
-
-  var totalScore = 0; // user's score
 
 	var wins = 0;
 	var losses = 0;
+	
+	$("#winCount").html(wins);
+	$("#lossCount").html(losses);
 
-	// ******* FUNCTIONS *******
 
-	// pick a random number
-	function pickRandomNumber(arr) {
+	initialUserScore = 0;
+	$("#userScore").html(initialUserScore);
 
-		var x = arr[Math.floor(Math.random() * arr.length)];
-		randNumber = x;
-		$("#randomNumber").html(randNumber);
+	
+	var targetNumChooser = function () {
+		return Math.floor(Math.random()*(120-19+1)+19);
+	};
 
-		console.log("random number: " + randNumber);
 
-	} // END of pickRandomNumber function
+	var targetNum = targetNumChooser();
 
-	// pick random numbers for crystals
+	$("#targetNum").html(targetNum);
 
-	function pickRandomCrystals(arr) {
+	
+	var crystalNum1= 10;
+	var crystalNum2 = 5;
+	var crystalNum3 = 8;
+	var crystalNum4 = 1;
 
-		for (var y = 0; y < 4; y++){
+	
+	$("#crystal1").on("click", function() {
 
-			var a = arr[Math.floor(Math.random() * arr.length)];
+		
+		initialUserScore += crystalNum1;
+		$("#userScore").html(initialUserScore);
 
-			crystalNumbers.push(a);
-		}
-    // check which numbers have been picked
-		console.log("crystal numbers: " + crystalNumbers);
-
-	} // END of pickRandomCrystals function
-
-	function crystalValues(arr) {
-
-		// change value of each crystal button according to array
-		for (i = 0; i < arr.length; i++) {
-
-		$("#button-" + (i+1)).attr("value", arr[i]);
-		console.log(this);
-		}
-		c1 = arr[0];
-		c2 = arr[1];
-		c3 = arr[2];
-		c4 = arr[3];
-	} // END of crystalValues function
-
-	function gameReset(x) {
-
-		crystalNumbers = []; // clears crystal number values
-
-		pickRandomNumber(rand);
-
-		pickRandomCrystals(crystals);
-
-		crystalValues(crystalNumbers);
-
-		totalScore = 0;
-		$("#totalNumber").html(totalScore);
-
-		alert(x);
-	} // END of gameReset function
-
-	// *** GAME SETTINGS AT START ***
-
-	pickRandomNumber(rand); // random number to match
-	pickRandomCrystals(crystals); // array of random crystal values
-	crystalValues(crystalNumbers);
-
-		// crystal button functions
-
-		$("#button-1").on("click", function() {
-
-			totalScore += c1;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-2").on("click", function() {
-
-			totalScore += c2;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-3").on("click", function() {
-
-			totalScore += c3;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-4").on("click", function() {
-
-			totalScore += c4;
-			$("#totalNumber").html(totalScore);
-		});
-
-	$("button").on("click", function() {
-		// this is what happens if the user wins
-		if (totalScore == randNumber) {
-
+	
+		if (initialUserScore === targetNum) {
+			
+			initialUserScore = 0;
 			wins++;
-			console.log(totalScore);
-			$("#totalNumber").html(totalScore);
-			$("#wins").html("Wins: " + wins);
-
-
-			setTimeout(function() {gameReset("Congrats, YOU WIN!!")}, 200);
+			$("#winCount").html(wins);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU WIN, congratulations!!");
 		}
 
-		else if (totalScore > randNumber){
-
+	
+		if (initialUserScore > targetNum) {
+			initialUserScore = 0;
 			losses++;
-			$("#totalNumber").html(totalScore);
-			$("#losses").html("Losses: " + losses);
-
-			setTimeout(function() {gameReset("YOU LOSE...Thank You Come Again!")}, 200);
+			$("#lossCount").html(losses);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU LOSE, try again!");
 		}
+
 	});
 
-});
+	
+
+	$("#crystal2").on("click", function() {
+
+		initialUserScore += crystalNum2;
+		$("#userScore").html(initialUserScore);
+
+		if (initialUserScore === targetNum) {
+			initialUserScore = 0;
+			wins++;
+			$("#winCount").html(wins);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU WIN, congratulations!!");
+		}
+
+		if (initialUserScore > targetNum) {
+			initialUserScore = 0;
+			losses++;
+			$("#lossCount").html(losses);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU LOSE, try again!");
+		}
+
+	});
+
+	$("#crystal3").on("click", function() {
+
+		initialUserScore += crystalNum3;
+		$("#userScore").html(initialUserScore);
+
+		if (initialUserScore === targetNum) {
+			initialUserScore = 0;
+			wins++;
+			$("#winCount").html(wins);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU WIN, congratulations!!");
+		}
+
+		if (initialUserScore > targetNum) {
+			initialUserScore = 0;
+			losses++;
+			$("#lossCount").html(losses);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU LOSE, try again!");
+		}
+
+	});
+
+	$("#crystal4").on("click", function() {
+
+		initialUserScore += crystalNum4;
+		$("#userScore").html(initialUserScore);
+
+		if (initialUserScore === targetNum) {
+			initialUserScore = 0;
+			wins++;
+			$("#winCount").html(wins);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU WIN, congratulations!!");
+		}
+
+		if (initialUserScore > targetNum) {
+			initialUserScore = 0;
+			losses++;
+			$("#lossCount").html(losses);
+			targetNum = targetNumChooser();
+			$("#targetNum").html(targetNum);
+			alert("YOU LOSE, try again!");
+			
+		}
+
+		
+		});
+
+	});
